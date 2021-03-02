17ba5dff96fed91a46beeb305ad21109a411de2d
brovetto <77513914+brovetto@users.noreply.github.com>
2021-03-02 14:03:15 -0800

    Week 7

--- _js/gameTemplate.js
+++ _js/gameTemplate.js
@@ -2,33 +2,37 @@
 // https://eloquentjavascript.net/code/chapter/17_canvas.js
 // https://developer.mozilla.org/en-US/docs/Web/API/Element/mousemove_event
 
-//initializing global variables to create a canvas
+//initializing GLOBAL variables to create a canvas
 let canvasDiv;
 let canvas;
 let ctx;
+
+// lets us know if game is initialized
 let initialized = false;
-// variables used to alter and intitalize the canvas we are making 
 
 // setup mouse position variables
 let mouseX = 0;
 let mouseY = 0;
-//object
+
+// object setting mousePos
+let mousePos = {
+  x: 0,
+  y: 0
+};
+
+let mouseClicks = {
+  x: 0,
+  y: 0
+};
+
 let mouseClickX = 0;
 let mouseClickY = 0;
-//sets variable to be 0 and they stay until we click 
-//assigns values to classes and properties 
 
 
-
-
-
-
-//assign 
 function init() {
   // create a new div element
   canvasDiv = document.createElement("div");
   canvasDiv.id = "chuck";
-  //chuck changes and access elements in css 
   // and give it some content
   canvas = document.createElement('canvas');
   // add the text node to the newly created div
@@ -38,34 +42,103 @@ function init() {
   document.body.insertBefore(canvasDiv, currentDiv);
   canvas.width = 500;
   canvas.height = 500;
-  document.getElementById("chuck").style.width = canvas.width +'px';
-  document.getElementById("chuck").style.height = canvas.height +'px';
-  //pulling info CSS 
+  document.getElementById("chuck").style.width = canvas.width + 'px';
+  document.getElementById("chuck").style.height = canvas.height + 'px';
   ctx = canvas.getContext('2d');
   initialized = true;
 }
 
 // create an object to hold attributes in order to draw a shape on canvas
 // more comments
-//an object we know by colons and h and w 
-let mySquare = {
+let oSquare = {
+  w: 50,
+  h: 50,
+  x: 300,
+  y: 300,
+  // comments not here...
+  vx: 0.1,
+  vy: 0.1,
+  color: 'black'
+};
+
+// create a constructor function that allows you to create more of one type of object
+function fSquare(w,h,x,y, vx, vy){
+  this.w = w;
+  this.h = h;
+  this.x = x;
+  this.y = y;
+  this.vx = 0;
+  this.vy = vy;
+  this.color = c;
+  this.update = 
+  this.draw = function(){
+      ctx.fillStyle = this.color;
+      ctx.fillRect(this.x, this.y, this.w, this.h);
+      ctx.strokeRect(this.x, this.y, this.w, this.h);
+  };
+}
+
+// create a JS Class that allow you to create more objects from a 'template' using 'new'
+class cSquare {
+  constructor(w, h, x, y) {
+    this.w = w;
+    this.h = h;
+    this.x = x;
+    this.y = y;
+    this.vx = 0;
+    this.vy = 0;
+    this.color = 'black';  
+  }
+  draw(){
+    ctx.fillStyle = this.color;
+      ctx.fillRect(this.x, this.y, this.w, this.h);
+      ctx.strokeRect(this.x, this.y, this.w, this.h);
+  }
+}
+
+// create new object from existing object oSquare
+let newSquare = Object.create(oSquare);
+
+// create instance of constructor FUNCTION fSquare
+let oneSquare = new fSquare(25,25,0,0,0,0,'red' );
+let twoSquare = new fSquare(25,25,150,0,0,0,'green' );
+let threeSquare = new fSquare(25,25,0,0,0,0,'blue' );
+
+
+// create instance of class cSquare
+let anotherSquare = new cSquare (40, 40, 25, 25);
+
+
+
+let myCircle = {
+  r: 25,
   w: 50,
   h: 50,
   x: 150,
   y: 200,
   // comments not here...
-  vx: 3,
-  vy: 0,
-  //vx and vy alters how fast they appear on the screen 
-  color: 'black'
-  // color alters the color 
+  vx: 0.1,
+  vy: 0.1,
+  color: 'grey',
+  draw: function(){
+  ctx.fillStyle = myCircle.color;
+  ctx.beginPath();
+  ctx.arc(myCircle.x, myCircle.y, myCircle.r, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.fill();
+  }
 };
-//mouse position using call back function ??
+
+
 // gets mouse position when clicked
 addEventListener('mousemove', e => {
-    mouseX = e.offsetX;
-    mouseY = e.offsetY;
-    //we are gonna use this later 
+  mouseX = e.offsetX;
+  mouseY = e.offsetY;
+  // we're gonna use this
+  mousePos = {
+    x: mouseX,
+    y: mouseY
+  };
 });
 
 // gets mouse position when clicked
@@ -75,14 +148,46 @@ function mouseClick(e) {
   console.log(`Screen X/Y: ${e.screenX}, ${e.screenY}, Client X/Y: ${e.clientX}, ${e.clientY}`);
   mouseClickX = e.clientX;
   mouseClickY = e.clientY;
-  return [mouseClickX, mouseClickY];
+  mouseClicks = {
+    x: mouseClickX,
+    y: mouseClickY
+  };
+}
+
+function collide(a, b, xd, yd) {
+  if (a.x  <= b.x &&
+    b.x <= a.x + a.w &&
+    a.y <= b.y &&
+    b.y <= a.y + a.h
+  ) {
+    console.log('collided');
+    return true;
+  }
 }
 
 // updates all elements on canvas
-function update() {
-  mySquare.x += mySquare.vx;
-  if (mySquare.x + mySquare.w > canvas.width || mySquare.x < 0) {
-    mySquare.vx *= -1;
+function update(mod) {
+  if (collide(oSquare, myCircle)) {
+    oSquare.color = 'red';
+    myCircle.color = 'blue';
+    console.log('circle collided')
+  }
+  if (collide(oSquare, mousePos)) {
+    oSquare.color = 'red';
+    oSquare.vx += 1;
+  }
+  else{
+    oSquare.color = 'orange';
+  }
+  oSquare.x += oSquare.vx * mod;
+  oSquare.y += oSquare.vy * mod;
+  if (oSquare.x + oSquare.w >= canvas.width || oSquare.x <= 0) {
+    oSquare.vx *= -1;
+    oSquare.color = 'blue';
+  }
+  if (oSquare.y + oSquare.h >= canvas.height || oSquare.y <= 0) {
+    oSquare.vy *= -1;
+    oSquare.color = 'green';
   }
 }
 
@@ -97,9 +202,9 @@ function drawText(color, font, align, base, text, x, y) {
 
 // draws a square, circle, or rectangle
 function drawSquare() {
-  ctx.fillStyle = mySquare.color;
-  ctx.fillRect(mySquare.x, mySquare.y, mySquare.w, mySquare.h);
-  ctx.strokeRect(mySquare.x, mySquare.y, mySquare.w, mySquare.h);
+  ctx.fillStyle = oSquare.color;
+  ctx.fillRect(oSquare.x, oSquare.y, oSquare.w, oSquare.h);
+  ctx.strokeRect(oSquare.x, oSquare.y, oSquare.w, oSquare.h);
 }
 
 // function drawCircle() {
@@ -115,28 +220,30 @@ function draw() {
   // clears the canvas before drawing
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawText('black', "24px Helvetica", "left", "top", "FPS: " + fps, 400, 0);
+  drawText('black', "24px Helvetica", "left", "top", "Delta: " + gDelta, 400, 32);
   drawText('black', "24px Helvetica", "left", "top", "mousepos: " + mouseX + " " + mouseY, 0, 0);
   drawText('black', "24px Helvetica", "left", "top", "mouseclick: " + mouseClickX + " " + mouseClickY, 0, 32);
   drawSquare();
+  oneSquare.draw();
+  anotherSquare.draw();
+  myCircle.draw();
 }
 
 // set variables necessary for game loop
-let delta;
 let fps;
-let gameDelta;
+let now;
+let delta;
+let gDelta;
 let then = performance.now();
 
 //main game loop
 function main() {
-    //these are the vAariables declared locally inside of function 
-  let now = performance.now();
-  //tells the game to run 
+  now = performance.now();
   delta = now - then;
-  gameDelta = Math.min(delta, 0.25);
-  //slows down FPS 
-  fps = (Math.floor(1000 / delta));
+  gDelta = (Math.min(delta, 17));
+  fps = Math.ceil(1000 / gDelta);
   if (initialized) {
-    update();
+    update(gDelta);
     draw();
   }
   then = now;
